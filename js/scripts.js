8ff87ff9f3afa44ba9fb374e4f034d6488e75a53
nkhakwan <post_khan@yahoo.com>
2020-03-23 16:25:41 -0700

    finalized. Its more of practicing syntax to memorize it than to actually make a project

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+$(document).ready(function(){
+
+
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
